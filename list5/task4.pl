38655579f4898ef02e01d10af9b2f4b077dcb2b9
Kamil Król <kamil55525@gmail.com>
2019-06-09 21:51:03 +0200

    Add list5

---
+++ list5/task4.pl
@@ -0,0 +1,32 @@
+﻿  use HTTP::Daemon;
+  use HTTP::Status;  
+  #use IO::File;
+
+  my $d = HTTP::Daemon->new(
+           LocalAddr => 'localhost',
+           LocalPort => 4321,
+       )|| die;
+  
+  print "Please contact me at: <URL:", $d->url, ">\n";
+
+
+  while (my $c = $d->accept) {
+      while (my $r = $c->get_request) {
+          if ($r->method eq 'GET') {
+            print "HEADERS: \n";
+            print $r->headers_as_string;
+
+            $response = HTTP::Response->new();
+            $response->content($r->headers_as_string);
+            print "RESPONSE: \n";
+            print $response->as_string;
+            $c->send_response($response);
+          }
+          else {
+              $c->send_error(RC_FORBIDDEN)
+          }
+
+      }
+      $c->close;
+      undef($c);
+  }
---
+++ list5/www-service/server.pl
@@ -0,0 +1,33 @@
+﻿  use HTTP::Daemon;
+  use HTTP::Status;  
+  #use IO::File;
+
+  my $d = HTTP::Daemon->new(
+           LocalAddr => '192.168.137.1',
+           LocalPort => 4321,
+       )|| die;
+  
+  print "Please contact me at: <URL:", $d->url, ">\n";
+
+
+  while (my $c = $d->accept) {
+      while (my $r = $c->get_request) {
+            print "FILENAME:", $r->uri->as_string,"\n";
+          if ($r->method eq 'GET') {
+            my $arg = $r->uri->path;
+            if ($arg eq "/") {
+                $file_s= "./index.html";
+                $c->send_file_response($file_s);
+            } else {
+                $file_s= "./".$arg;
+                $c->send_file_response($file_s);
+            }
+          }
+          else {
+              $c->send_error(RC_FORBIDDEN)
+          }
+
+      }
+      $c->close;
+      undef($c);
+  }
